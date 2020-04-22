048d24e2f0c48b41fbe42b051f2ff7af3d8216bc
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-04-22 09:30:54 +0900

    いったん最終課題に集中

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user 
+  
+  validates :text, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
   root to: 'posts#index'
   resources :users, only: [:edit, :update]
+  resources :posts, only: [:index, :new, :create]
 end
