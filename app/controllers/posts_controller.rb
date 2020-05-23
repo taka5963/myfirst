a183a097ae445ac6e2be093e65a2a7d06b8d0a1c
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-05-23 21:54:52 +0900

    必要最低限の機能の実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.all
+    # @posts = Post.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -9,11 +12,23 @@ class PostsController < ApplicationController
   
   def create
     Post.create(post_params)
+    # redirect_to root_path
   end
+
+  def show
+    @posts = Post.all
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
+  end
+
 
   private
   def post_params
-    params.require(:post).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id]) 
   end
 
 end
